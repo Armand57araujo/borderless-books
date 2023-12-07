dddf23ebcfccc54304705c306cff9e5dfab7edbc
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-06 20:14:10 -0800

    more changes

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -8,7 +8,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [loginUserMutation] = useMutation(LOGIN_USER);
+  const [loginUserMutation, {error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
