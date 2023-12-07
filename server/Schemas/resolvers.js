6119f8e978a8495ba73028ee9c4400398107c1ac
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-06 22:23:50 -0800

    added more code to resolvers

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,28 +1,87 @@
-// Import your Mongoose models or functions here
+const { User, Book } = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
+
+// Helper function to generate JWT token
+const generateToken = (user) => {
+  return jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1h' });
+};
 
 const resolvers = {
-    Query: {
-      // Implement the 'me' resolver
-      me: () => {
-        // Logic to get the current user
-      },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('books');
+      }
+      throw AuthenticationError;
     },
-    Mutation: {
-      // Implement login, addUser, saveBook, and removeBook resolvers
-      login: (_, { email, password }) => {
-        // Logic for user login
-      },
-      addUser: (_, { username, email, password }) => {
-        // Logic to add a new user
-      },
-      saveBook: (_, { input }) => {
-        // Logic to save a book to a user's profile
-      },
-      removeBook: (_, { bookId }) => {
-        // Logic to remove a book from a user's profile
-      },
+  },
+
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new Error('Invalid password');
+      }
+
+      const token = generateToken(user);
+      return { token, user };
     },
-  };
-  
-  module.exports = resolvers;
-  
+
+    addUser: async (_, { username, email, password }) => {
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        throw new Error('User already exists');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({ username, email, password: hashedPassword });
+      const token = generateToken(newUser);
+      return { token, user: newUser };
+    },
+
+    saveBook: async (_, { input }, { user }) => {
+      if (user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeBook: async (_, { bookId }, { user }) => {
+      if (!user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      const currentUser = await User.findById(user._id);
+      if (!currentUser) {
+        throw new Error('User not found');
+      }
+
+      const removedBook = await Book.findByIdAndRemove(bookId);
+      if (!removedBook) {
+        throw new Error('Book not found');
+      }
+
+      currentUser.books.pull(bookId);
+      await currentUser.save();
+
+      return removedBook;
+    },
+  },
+};
+
+module.exports = resolvers;
